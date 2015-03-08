0f89862f00c76fcfb1d63cc9e017ae73ab263af8
Richard Munroe <rimunroe@gmail.com>
2015-03-07 20:01:51 -0500

    removed react from deps

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 var creator = require('./view.coffee');
 
-module.exports = {
-  name: 'pipeline-react-views',
-  factories: {
-    view: creator
-  }
+module.exports = function(React){
+  return {
+    name: 'pipeline-react-views',
+    factories: {
+      view: creator(React)
+    }
+  };
 };
